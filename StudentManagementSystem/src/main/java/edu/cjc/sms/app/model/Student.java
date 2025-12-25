51d94d9fed2f07892560a978ea44ae7e9dfdc0f9
hp <hp@suchel>
2025-12-25 23:55:41 +0530

    second day work

---
+++ StudentManagementSystem/src/main/java/edu/cjc/sms/app/model/Student.java
@@ -0,0 +1,22 @@
+package edu.cjc.sms.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int studentId;
+	private String studentFullName;
+	private String studentEmail;
+	private int studentAge;
+	private String studentCollegeName;
+	private String batchNumber;
+	private String batchMode;
+	private String studentCourse;
+	private double feesPaid;
+}
